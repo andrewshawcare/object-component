e7a4c23590da6f84d25353eaeba99442f48338b4
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-03-30 08:55:10 -0500

    Add valueComponent

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-ObjectComponent = function (object) {
+ObjectComponent = function (object, valueComponent) {
+  var valueComponent = valueComponent || function (value) {
+    var valueElement = document.createElement("span");
+    valueElement.innerText = value;
+    return valueElement;
+  };
   var objectElement = document.createElement("dl");
   objectElement.classList.add("object");
   Object.keys(object).forEach(function (key) {
-    var keyElement = document.createElement("dt");
-    keyElement.classList.add("key");
-    keyElement.innerText = key;
-    objectElement.appendChild(keyElement);
+    var termElement = document.createElement("dt");
+    termElement.classList.add("term");
+    termElement.innerText = key;
+    objectElement.appendChild(termElement);
 
-    var valueElement = document.createElement("dd");
-    valueElement.classList.add("value");
-    valueElement.innerText = object[key];
-    objectElement.appendChild(valueElement);
+    var descriptionElement = document.createElement("dd");
+    descriptionElement.classList.add("description");
+    descriptionElement.appendChild(valueComponent(object[key]));
+    objectElement.appendChild(descriptionElement);
   });
   return objectElement;
 };
