667cb4088c01f7caaf545e45f39c87638ac5b762
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-05-22 14:22:33 -0500

    Update component to use component workshop

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-ObjectComponent = function (object, valueComponent) {
-  var valueComponent = valueComponent || function (value) {
-    var valueElement = document.createElement("span");
-    valueElement.innerText = value;
-    return valueElement;
-  };
-  var objectElement = document.createElement("dl");
-  objectElement.classList.add("object");
-  Object.keys(object).forEach(function (key) {
-    var termElement = document.createElement("dt");
-    termElement.classList.add("term");
-    termElement.innerText = key;
-    objectElement.appendChild(termElement);
+/*global define, document */
+define([], function () {
+  "use strict";
+  return function (data) {
+    var data = data || {};
+    var object = data.object || {};
+    var valueComponent = data.valueComponent || function (value) {
+      var valueElement = document.createElement("span");
+      valueElement.innerText = value;
+      return valueElement;
+    };
 
-    var descriptionElement = document.createElement("dd");
-    descriptionElement.classList.add("description");
-    descriptionElement.appendChild(valueComponent(object[key]));
-    objectElement.appendChild(descriptionElement);
-  });
-  return objectElement;
-};
+    var objectElement = document.createElement("dl");
+    objectElement.classList.add("object");
+
+    Object.keys(object).forEach(function (key) {
+      var termElement = document.createElement("dt");
+      termElement.classList.add("term");
+      termElement.innerText = key;
+      objectElement.appendChild(termElement);
+
+      var descriptionElement = document.createElement("dd");
+      descriptionElement.classList.add("description");
+      descriptionElement.appendChild(valueComponent(object[key]));
+      objectElement.appendChild(descriptionElement);
+    });
+
+    return objectElement;
+  };
+});
